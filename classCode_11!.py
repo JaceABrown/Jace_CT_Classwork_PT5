34dcbcffa5a9c9dc879cf69141d5521397fd4d1e
JaceABrown <79872355+JaceABrown@users.noreply.github.com>
2021-04-08 12:27:18 -0700

    Add files via upload

--- classCode_11!.py
+++ classCode_11!.py
@@ -11,6 +11,7 @@ def mainProgram():
  2. add a bunch of numbers
  2. Return the value at an index position!
  3. Random Search
+ 6. print contents of list 
  4. Quit Program""")
             if choice =="1":
                 addToList()
@@ -20,8 +21,9 @@ def mainProgram():
                 randomSearch()
             elif choice == "4":
                 randomSearch
-
             elif choice == "5":
+                print(myList)
+            elif choice == "6":
                 print(myList)
             else:
                 break
@@ -38,7 +40,7 @@ def addABunch():
     print("we are adding a bunch of intergers here! ")
     numToAdd = input("how many new intergers would you like to add!")
     numRange = input("and how high would you like these numbers to go?")
-    for x in range(0, int(numToAdd):
+    for x in range(0, int(numToAdd)):
         myList.append(random.randint(0, int(numRange)))
     print("your list is now complete.")
 
@@ -54,9 +56,9 @@ def randomSearch():
     print(myList[random.randint(0, len(myList)-1)])
 
 def linearSearch():
-    print(we'er gonna check out each item one at a time in your list!  this sucks")
-          serchItem = input("what you looking for, pardner")
-          for x in range(len(myList)):
+    print("we'er gonna check out each item one at a time in your list!  this sucks")
+    serchItem = input("what you looking for, pardner")
+    for x in range(len(myList)):
               
 
 
